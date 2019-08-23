ddb68f0aa01679fb4b713c0f64aedeacce97f7b1
gayatrianuhya <54269301+gayatrianuhya@users.noreply.github.com>
2019-08-23 15:11:31 +0530

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-public class Cucumcasestudy {
+public class Stepdefs {
 	 static WebDriver driver;
 	@Given("TestMe app is launched")
 	public void testme_app_is_launched() {
